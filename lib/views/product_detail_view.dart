14c7f4f0ece72977db6049490a64a70ca98b8bb4
loficrypto <lofiking810@gmail.com>
2024-10-16 19:50:28 +0600

    Create product_detail_view.dart

---
+++ lib/views/product_detail_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductDetailView extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailView({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(product.imageUrl),
+            SizedBox(height: 16),
+            Text(
+              product.name,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Text(product.description),
+            SizedBox(height: 16),
+            Text('Price: \$${product.price}'),
+            Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                // Add to cart functionality
+              },
+              child: Text('Add to Cart'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
