2f7a7e68c4841cfa78f82121b1f76020fd639454
loficrypto <lofiking810@gmail.com>
2024-10-16 20:00:49 +0600

    Create product_card.dart

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final VoidCallback onTap;
+
+  ProductCard({required this.product, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('\$${product.price.toString()}'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
